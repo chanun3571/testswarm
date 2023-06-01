ed5e54c9ba547d017b95919188b2576b3dd07b2e
chanun3571 <110588578+chanun3571@users.noreply.github.com>
2023-06-01 19:06:13 +0700

    Add files via upload

---
+++ main/arobota2/script/pose_collector.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import rospy
+from geometry_msgs.msg import Pose, PoseStamped, PoseWithCovarianceStamped
+from geometry_msgs.msg import PoseArray
+
+class PoseCollector:
+    def __init__(self):
+        rospy.init_node("poseCollector", anonymous=True)
+
+        # ROS Initialize
+        # Number of Agents = 3
+        rospy.Subscriber("/robot1/amcl_pose", PoseWithCovarianceStamped, self.allpose1_callback)
+        rospy.Subscriber("/robot2/amcl_pose", PoseWithCovarianceStamped, self.allpose2_callback)
+        rospy.Subscriber("/robot3/amcl_pose", PoseWithCovarianceStamped, self.allpose3_callback)
+        self.pub_allPosition = rospy.Publisher("/allPos", PoseArray, queue_size=1)
+        self.pub_allOrientation = rospy.Publisher("/allOrien", PoseArray, queue_size=1)
+
+        self.CollectorPosi = [[0,0],[0,0],[0,0]]
+        self.CollectorOrien =[[0,0],[0,0],[0,0]]
+        
+    def allpose1_callback(self, msg):
+        msg.pose.pose.position.x=self.CollectorPosi[0][0]
+        msg.pose.pose.position.y=self.CollectorPosi[0][1]
+        msg.pose.pose.orientation.z=self.CollectorOrien[0][0]
+        msg.pose.pose.orientation.w=self.CollectorOrien[0][1]
+    def allpose2_callback(self, msg):
+        msg.pose.pose.position.x=self.CollectorPosi[1][0]
+        msg.pose.pose.position.y=self.CollectorPosi[1][1]
+        msg.pose.pose.orientation.z=self.CollectorOrien[1][0]
+        msg.pose.pose.orientation.w=self.CollectorOrien[1][1]
+    def allpose2_callback(self, msg):
+        msg.pose.pose.position.x=self.CollectorPosi[2][0]
+        msg.pose.pose.position.y=self.CollectorPosi[2][1]
+        msg.pose.pose.orientation.z=self.CollectorOrien[2][0]
+        msg.pose.pose.orientation.w=self.CollectorOrien[2][1]
+
+    def spin(self):
+        while not rospy.is_shutdown():
+            self.pub_allPosition.publish(self.CollectorPosi)
+            self.pub_allOrientation.publish(self.CollectorOrien)
+            self.CollectorPosi = [[0,0],[0,0],[0,0]]
+            self.CollectorOrien =[[0,0],[0,0],[0,0]]
+            self.Rate(10).sleep()
+
+if __name__ == "__main__":
+    try:
+        posecollector = PoseCollector()
+        posecollector.spin()
+    except rospy.ROSInterruptException:
+        pass
---
+++ main/arobota2/script/pose_collector_array.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+
+import actionlib, rospy
+from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
+from geometry_msgs.msg import PoseWithCovarianceStamped, PoseArray,Pose,Quaternion,Twist,Point
+
+rospy.init_node('allPose')
+class PoseCollector:
+    def __init__(self):
+        rospy.Subscriber("/robot1/amcl_pose", PoseWithCovarianceStamped, self.allpose1_callback)
+        rospy.Subscriber("/robot2/amcl_pose", PoseWithCovarianceStamped, self.allpose2_callback)
+        rospy.Subscriber("/robot3/amcl_pose", PoseWithCovarianceStamped, self.allpose3_callback)
+        self.poseArrayPub = rospy.Publisher('/allpose', PoseArray, queue_size=1)
+        self.locations = dict()
+
+    def allpose1_callback(self, msg):       
+        self.locations['robot1'] = msg.pose.pose
+    def allpose2_callback(self, msg):
+        self.locations['robot2'] = msg.pose.pose
+    def allpose3_callback(self, msg):
+        self.locations['robot3'] = msg.pose.pose
+
+# create a function that represnt posearray in Rviz so we could visualize the waypoints 
+    def wayPoints(self, waypointsList):
+        self.waypoints = PoseArray()
+        self.waypoints.header.frame_id = 'map'
+        self.waypoints.header.stamp= rospy.Time.now()
+        self.waypointPoses = []
+        for key, value in waypointsList.items():
+            self.waypointPoses.append(waypointsList[key])
+        self.waypoints.poses = self.waypointPoses
+        self.poseArrayPub.publish(self.waypoints)
+
+    def spin(self):
+        # initialize message
+        while not rospy.is_shutdown():
+            rate = rospy.Rate(10)
+            self.wayPoints(self.locations)
+            rate.sleep()
+            
+if __name__=='__main__':
+    try:
+        agent=PoseCollector()
+        agent.spin()
+    except rospy.ROSInterruptException:
+        pass
